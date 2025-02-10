fd3af5d614442ac02f0b4ee62c60a1793417f021
singhdevhub-lovepreet <lovepreet@singhdevhub.com>
2025-02-10 18:46:38 +0000

    Adding few more files

---
+++ src/c/14_Memory_Addressses_Pointers.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int pointers(){    
+    
+    // pointer is just a memory address and nothing else. It is just an another type of data
+    int age = 40;
+    int* agePointer = &age;
+    printf("%p\n", agePointer);
+
+    double gpa = 4.0;
+    double *gpaPointer = &gpa;
+    char grade = 'A';
+    char *gradePointer = &grade;
+
+    // discuss about pointer in array, 
+    // pointer of pointer in 2D arrays, pointer algebra
+    
+    //dereferencing the pointer
+    printf("Age is: %d\n", *agePointer);
+    
+}
+
+int nd_pointer_arrays(){
+    int **nums[2][2] = {{1,2}, {3,4}};
+    for(int i = 0; i < sizeof(*nums)/4; i++){
+        for (int j = 0; j < sizeof(**nums)/4; j++)
+        {
+            printf("elements are : %d", **nums[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+int main(){
+    int age = 30;
+    double gpa = 4.0;
+    char grade = 'A';
+
+    printf("Address of age is %p\n", &age); // p = pointer and it gives virtual memory address
+    pointers();
+    nd_pointer_arrays();
+    
+    // bydefault return value is specified as 0
+}
---
+++ src/c/16_File_IO.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    FILE * fpointer = fopen("test.txt", "w"); // absolute path, relative path
+    
+    fprintf(fpointer, "I am a disco dancer");
+    // use append "a", "r" modes too
+    fclose(fpointer);
+
+    FILE * fpointer1 = fopen("test.txt", "w");
+    char line[255];
+    fgets(line, 255, fpointer1);
+    printf("%s\n", line);
+    fclose(fpointer1);
+}
+
+
+
+// Next: Union, typedef, #define CIRCLE_AREA(r) (3.14 * r * r) 
+// C-language compilation internals, c_cpp_properties, UTM/VM and use in vscode
